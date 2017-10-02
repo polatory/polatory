542fdf3c3ed76c8b8c5e8e9cf101e294dd27ad42
Masashi Mizuno <mizuno@gsinet.co.jp>
2017-10-02 14:02:05 +0900

    Clean common headers.

--- include/polatory/common/utility.hpp
+++ include/polatory/common/utility.hpp
@@ -2,16 +2,17 @@
 
 #pragma once
 
+#include <functional>
 #include <utility>
 
 namespace polatory {
 namespace common {
 
 template <class T, class Compare = std::less<T>>
-auto make_sorted_pair(const T& first, const T& second, Compare comp = Compare()) {
+auto make_sorted_pair(T&& first, T&& second, Compare comp = Compare()) {
   return comp(first, second)
-         ? std::make_pair(first, second)
-         : std::make_pair(second, first);
+         ? std::make_pair(std::forward<T>(first), std::forward<T>(second))
+         : std::make_pair(std::forward<T>(second), std::forward<T>(first));
 }
 
 } // namespace common
