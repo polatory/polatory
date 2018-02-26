df24aaaa7ef8c497eb740ee617f7b0de4cb3eba9
mizuno-gsinet <mizuno@gsinet.co.jp>
2018-02-26 15:17:58 +0900

    Parallelize normal estimation. (#29)

--- src/point_cloud/normal_estimator.cpp
+++ src/point_cloud/normal_estimator.cpp
@@ -17,47 +17,57 @@ normal_estimator::normal_estimator(const geometry::points3d& points)
   , tree_(points, true) {
 }
 
-const normal_estimator& normal_estimator::estimate_with_knn(size_t k, double plane_factor_threshold) {
-  std::vector<size_t> nn_indices;
-  std::vector<double> nn_distances;
-
+normal_estimator& normal_estimator::estimate_with_knn(size_t k, double plane_factor_threshold) {
   normals_ = geometry::vectors3d(n_points_, 3);
-  for (size_t i = 0; i < n_points_; i++) {
-    std::tie(nn_indices, nn_distances) = tree_.knn_search(points_.row(i), k);
 
-    normals_.row(i) = estimate_impl(nn_indices, plane_factor_threshold);
+#pragma omp parallel
+  {
+    std::vector<size_t> nn_indices;
+    std::vector<double> nn_distances;
+#pragma omp for
+    for (size_t i = 0; i < n_points_; i++) {
+      geometry::point3d p = points_.row(i);
+      std::tie(nn_indices, nn_distances) = tree_.knn_search(p, k);
+
+      normals_.row(i) = estimate_impl(nn_indices, plane_factor_threshold);
+    }
   }
 
   return *this;
 }
 
-const normal_estimator& normal_estimator::estimate_with_radius(double radius, double plane_factor_threshold) {
-  std::vector<size_t> nn_indices;
-  std::vector<double> nn_distances;
-
+normal_estimator& normal_estimator::estimate_with_radius(double radius, double plane_factor_threshold) {
   normals_ = geometry::vectors3d(n_points_, 3);
-  for (size_t i = 0; i < n_points_; i++) {
-    std::tie(nn_indices, nn_distances) = tree_.radius_search(points_.row(i), radius);
 
-    normals_.row(i) = estimate_impl(nn_indices, plane_factor_threshold);
+#pragma omp parallel
+  {
+    std::vector<size_t> nn_indices;
+    std::vector<double> nn_distances;
+#pragma omp for
+    for (size_t i = 0; i < n_points_; i++) {
+      geometry::point3d p = points_.row(i);
+      std::tie(nn_indices, nn_distances) = tree_.radius_search(p, radius);
+
+      normals_.row(i) = estimate_impl(nn_indices, plane_factor_threshold);
+    }
   }
 
   return *this;
 }
 
-geometry::vectors3d normal_estimator::orient_by_outward_vector(const geometry::vector3d& v) const {
+geometry::vectors3d normal_estimator::orient_by_outward_vector(const geometry::vector3d& v) {
   if (n_points_ > 0 && normals_.rows() == 0)
     throw std::runtime_error("Normals have not been estimated yet.");
 
-  geometry::vectors3d normals = normals_;
-
-  for (auto n : common::row_range(normals)) {
+#pragma omp parallel for
+  for (size_t i = 0; i < n_points_; i++) {
+    auto n = normals_.row(i);
     if (n.dot(v) < 0.0) {
       n = -n;
     }
   }
 
-  return normals;
+  return normals_;
 }
 
 geometry::vector3d normal_estimator::estimate_impl(const std::vector<size_t>& nn_indices, double plane_factor_threshold) const {
