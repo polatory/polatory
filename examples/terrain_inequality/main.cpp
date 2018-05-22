20213d05e03a4359e74c56b9b394114e2c15a894
mizuno-gsinet <mizuno@gsinet.co.jp>
2018-05-22 15:19:43 +0900

    Add an example of inequality fitter.

---
+++ examples/terrain_inequality/main.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2016, GSI and The Polatory Authors.
+
+#include <exception>
+#include <iostream>
+#include <tuple>
+
+#include <polatory/polatory.hpp>
+
+#include "parse_options.hpp"
+
+using polatory::common::concatenate_cols;
+using polatory::common::take_cols;
+using polatory::common::valuesd;
+using polatory::geometry::points3d;
+using polatory::interpolant;
+using polatory::isosurface::isosurface_2d;
+using polatory::isosurface::rbf_field_function;
+using polatory::model;
+using polatory::point_cloud::distance_filter;
+using polatory::rbf::biharmonic;
+using polatory::read_table;
+
+int main(int argc, const char *argv[]) {
+  try {
+    auto opts = parse_options(argc, argv);
+
+    // Read points and normals.
+    auto table = read_table(opts.in_file);
+    points3d points = concatenate_cols(take_cols(table, 0, 1), valuesd::Zero(table.rows()));
+    valuesd values = table.col(2);
+    valuesd values_lb = table.col(3);
+    valuesd values_ub = table.col(4);
+
+    // Remove very close points.
+    std::tie(points, values, values_lb, values_ub) = distance_filter(points, opts.min_distance)
+      .filtered(points, values, values_lb, values_ub);
+
+    // Define model.
+    model model(biharmonic({ 1.0, opts.smooth }), opts.poly_dimension, opts.poly_degree);
+    interpolant interpolant(model);
+
+    interpolant.fit_inequality(points, values, values_lb, values_ub, opts.absolute_tolerance);
+    std::cout << "Number of RBF centers: " << interpolant.centers().rows() << std::endl;
+
+  	// Generate isosurface.
+    isosurface_2d isosurf(opts.mesh_bbox, opts.mesh_resolution);
+    rbf_field_function field_fn(interpolant);
+
+    points.col(2) = values;
+    isosurf.generate(field_fn)
+      .export_obj(opts.mesh_file);
+
+    return 0;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+}
---
+++ include/polatory/isosurface/isosurface_2d.hpp
@@ -0,0 +1,71 @@
+// Copyright (c) 2016, GSI and The Polatory Authors.
+
+#pragma once
+
+#include <cmath>
+#include <utility>
+#include <vector>
+
+#include <polatory/geometry/bbox3d.hpp>
+#include <polatory/geometry/point3d.hpp>
+#include <polatory/isosurface/rbf_field_function.hpp>
+#include <polatory/isosurface/surface.hpp>
+#include <polatory/isosurface/types.hpp>
+
+namespace polatory {
+namespace isosurface {
+
+class isosurface_2d {
+public:
+  isosurface_2d(const geometry::bbox3d& bbox, double resolution)
+    : bbox_(bbox)
+    , resolution_(resolution) {
+  }
+
+  surface generate(field_function& field_fn) const {  // NOLINT(runtime/references)
+    field_fn.set_evaluation_bbox(bbox_);
+
+    auto xmin = resolution_ * (std::floor(bbox_.min()(0) / resolution_) - 1.0);
+    auto ymin = resolution_ * (std::floor(bbox_.min()(1) / resolution_) - 1.0);
+    auto xmax = resolution_ * (std::ceil(bbox_.max()(0) / resolution_) + 1.0);
+    auto ymax = resolution_ * (std::ceil(bbox_.max()(1) / resolution_) + 1.0);
+    size_t nx = std::round((xmax - xmin) / resolution_) + 1;
+    size_t ny = std::round((ymax - ymin) / resolution_) + 1;
+
+    geometry::points3d points(ny * nx, 3);
+    for (size_t iy = 0; iy < ny; iy++) {
+      for (size_t ix = 0; ix < nx; ix++) {
+        points(iy * nx + ix, 0) = xmin + ix * resolution_;
+        points(iy * nx + ix, 1) = ymin + iy * resolution_;
+        points(iy * nx + ix, 2) = 0.0;
+      }
+    }
+    points.rightCols(1) = field_fn(points);
+
+    std::vector<geometry::point3d> vertices;
+    for (size_t i = 0; i < points.rows(); i++) {
+      vertices.push_back(points.row(i));
+    }
+
+    std::vector<face> faces;
+    for (size_t iy = 0; iy < ny - 1; iy++) {
+      for (size_t ix = 0; ix < nx - 1; ix++) {
+        auto i0 = iy * nx + ix;
+        auto i1 = iy * nx + (ix + 1);
+        auto i2 = (iy + 1) * nx + ix;
+        auto i3 = (iy + 1) * nx + (ix + 1);
+        faces.push_back({ i0, i1, i3 });
+        faces.push_back({ i0, i3, i2 });
+      }
+    }
+
+    return surface(std::move(vertices), std::move(faces));
+  }
+
+private:
+  const geometry::bbox3d bbox_;
+  const double resolution_;
+};
+
+}  // namespace isosurface
+}  // namespace polatory
