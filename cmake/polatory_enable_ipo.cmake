dd0a140f00839f5220b797895c3245ee1930a1b2
mizuno-gsinet <mizuno@gsinet.co.jp>
2018-09-26 12:21:17 +0900

    FIX IPO on MSVC.

--- cmake/polatory_enable_ipo.cmake
+++ cmake/polatory_enable_ipo.cmake
@@ -1,27 +1,26 @@
 ## # polatory_enable_ipo
 ## 
-## Enables interprocedural optimization for the target, if possible.
+## Enables interprocedural optimization for the target.
 function(polatory_enable_ipo TARGET)
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         return()
     endif()
 
-    if(MSVC)
-        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY
-            COMPILE_FLAGS " /GL "
-        )
-        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY
-            LINK_FLAGS " /LTCG "
-        )
-        message(STATUS "IPO is supported.")
+    # See https://gitlab.kitware.com/cmake/cmake/merge_requests/1721
+    if(CMAKE_VERSION VERSION_LESS 3.12)
+        if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+            target_compile_options(${TARGET} PRIVATE /GL)
+            target_link_libraries(${TARGET} PRIVATE -LTCG)
+            # NB: This variable also affects other targets in the same directory.
+            set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} /LTCG" PARENT_SCOPE)
+        endif()
     else()
         include(CheckIPOSupported)
         check_ipo_supported(RESULT ipo_supported OUTPUT output)
         if(ipo_supported)
             set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION TRUE)
-            message(STATUS "IPO is supported.")
         else()
-            message(STATUS "IPO is not supported: ${output}.")
+            message(WARNING "${output}")
         endif()
     endif()
 endfunction()
