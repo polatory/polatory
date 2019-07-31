bb6e94b11d761fa91d0beb9031e87f164019377c
mizuno-gsinet <mizuno@gsinet.co.jp>
2019-07-31 14:12:48 +0900

    The actual largest error is around 1.2e-6.

--- test/interpolation/random_transformation.hpp
+++ test/interpolation/random_transformation.hpp
@@ -15,9 +15,11 @@ polatory::geometry::affine_transformation3d random_transformation(seed_type seed
   std::mt19937 gen(seed);
   std::uniform_real_distribution<> angle_dist(0.0, 2.0 * polatory::common::pi<double>());
   std::uniform_real_distribution<> log_scale_dist(std::log10(0.1), std::log10(10.0));
+  std::uniform_real_distribution<> log_trans_dist(std::log10(1.0), std::log10(1e5));
 
   auto r = polatory::geometry::affine_transformation3d::roll_pitch_yaw({ angle_dist(gen), angle_dist(gen), angle_dist(gen) });
   auto s = polatory::geometry::affine_transformation3d::scaling({ std::pow(10.0, log_scale_dist(gen)), std::pow(10.0, log_scale_dist(gen)), std::pow(10.0, log_scale_dist(gen)) });
+  auto t = polatory::geometry::affine_transformation3d::translation({ std::pow(10.0, log_trans_dist(gen)), std::pow(10.0, log_trans_dist(gen)), std::pow(10.0, log_trans_dist(gen)) });
 
-  return r * s;
+  return t * r * s;
 }
