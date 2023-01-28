5362ca566c2e9304fffb01fdef6f5b0935a22652
unageek <29035331+unageek@users.noreply.github.com>
2023-01-29 00:22:44 +0900

    WIP

--- cmake/polatory_target_contents.cmake
+++ cmake/polatory_target_contents.cmake
@@ -1,5 +1,5 @@
 ## # polatory_target_contents
-## 
+##
 ## Copies files to the target directory after build.
 function(polatory_target_contents TARGET)
     foreach(FILE ${ARGN})
@@ -10,4 +10,9 @@ function(polatory_target_contents TARGET)
             $<TARGET_FILE_DIR:${TARGET}>
         )
     endforeach()
+
+    add_custom_command ( TARGET ${TARGET} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        $<TARGET_FILE:MKL::MKL> $<TARGET_FILE_DIR:${TARGET}>
+    )
 endfunction()
