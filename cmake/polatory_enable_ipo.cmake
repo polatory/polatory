c10096af10319b306301c851a7de1ce3529d606b
unageek <29035331+unageek@users.noreply.github.com>
2023-01-29 13:28:35 +0900

    Require CMake ≥ 3.16

--- cmake/polatory_enable_ipo.cmake
+++ cmake/polatory_enable_ipo.cmake
@@ -1,26 +1,16 @@
 ## # polatory_enable_ipo
-## 
+##
 ## Enables interprocedural optimization for the target.
 function(polatory_enable_ipo TARGET)
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         return()
     endif()
 
-    # IPO support for MSVC was added in CMake 3.12.
-    #   https://gitlab.kitware.com/cmake/cmake/issues/18189
-    #   https://gitlab.kitware.com/cmake/cmake/merge_requests/1721
-    # However, it was broken in CMake 3.14 and fixed in 3.15.
-    #   https://gitlab.kitware.com/cmake/cmake/issues/19571
-    if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") AND (CMAKE_VERSION VERSION_LESS 3.15))
-        target_compile_options(${TARGET} PRIVATE /GL)
-        target_link_libraries(${TARGET} PRIVATE -LTCG)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT ipo_supported OUTPUT output)
+    if(ipo_supported)
+        set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ON)
     else()
-        include(CheckIPOSupported)
-        check_ipo_supported(RESULT ipo_supported OUTPUT output)
-        if(ipo_supported)
-            set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ON)
-        else()
-            message(WARNING "${output}")
-        endif()
+        message(WARNING "${output}")
     endif()
 endfunction()
