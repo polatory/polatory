9bf6825f24fdf78da9b0a52b6f41b6ac2c1bb72d
unageek <29035331+unageek@users.noreply.github.com>
2023-09-20 14:41:14 +0900

    Extend `monomial_basis` for gradient data

--- include/polatory/polynomial/monomial_basis.hpp
+++ include/polatory/polynomial/monomial_basis.hpp
@@ -14,38 +14,61 @@ class monomial_basis : public polynomial_basis_base {
     POLATORY_ASSERT(degree >= 0 && degree <= 2);
   }
 
-  template <class Derived>
-  Eigen::MatrixXd evaluate(const Eigen::MatrixBase<Derived>& points) const {
-    auto n_points = points.rows();
+  template <class DerivedPoints>
+  Eigen::MatrixXd evaluate(const Eigen::MatrixBase<DerivedPoints>& points) const {
+    return evaluate(points, geometry::points3d(0, 3));
+  }
 
-    Eigen::MatrixXd result(basis_size(), n_points);
+  template <class DerivedPoints, class DerivedGradPoints>
+  Eigen::MatrixXd evaluate(const Eigen::MatrixBase<DerivedPoints>& points,
+                           const Eigen::MatrixBase<DerivedGradPoints>& grad_points) const {
+    auto mu = points.rows();
+    auto sigma = grad_points.rows();
+
+    Eigen::MatrixXd result(basis_size(), mu + dimension() * sigma);
 
     switch (dimension()) {
       case 1:
         switch (degree()) {
           case 0:
-            // 1
-            for (index_t i = 0; i < n_points; i++) {
-              result(0, i) = 1.0;
+            for (index_t i = 0; i < mu; i++) {
+              result(0, i) = 1.0;  // 1
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto i_x = mu + i;
+              result(0, i_x) = 0.0;  // 1_x
             }
             break;
+
           case 1:
-            // 1, x
-            for (index_t i = 0; i < n_points; i++) {
+            for (index_t i = 0; i < mu; i++) {
               auto p = points.row(i);
-              result(0, i) = 1.0;
-              result(1, i) = p(0);
+              result(0, i) = 1.0;   // 1
+              result(1, i) = p(0);  // x
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto i_x = mu + i;
+              result(0, i_x) = 0.0;  // 1_x
+              result(1, i_x) = 1.0;  // x_x
             }
             break;
+
           case 2:
-            // 1, x, x^2
-            for (index_t i = 0; i < n_points; i++) {
+            for (index_t i = 0; i < mu; i++) {
               auto p = points.row(i);
-              result(0, i) = 1.0;
-              result(1, i) = p(0);
-              result(2, i) = p(0) * p(0);
+              result(0, i) = 1.0;          // 1
+              result(1, i) = p(0);         // x
+              result(2, i) = p(0) * p(0);  // x^2
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto p = grad_points.row(i);
+              auto i_x = mu + i;
+              result(0, i_x) = 0.0;         // 1_x
+              result(1, i_x) = 1.0;         // x_x
+              result(2, i_x) = 2.0 * p(0);  // x^2_x
             }
             break;
+
           default:
             POLATORY_UNREACHABLE();
             break;
@@ -55,32 +78,65 @@ class monomial_basis : public polynomial_basis_base {
       case 2:
         switch (degree()) {
           case 0:
-            // 1
-            for (index_t i = 0; i < n_points; i++) {
-              result(0, i) = 1.0;
+            for (index_t i = 0; i < mu; i++) {
+              result(0, i) = 1.0;  // 1
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto i_x = mu + 2 * i;
+              auto i_y = mu + 2 * i + 1;
+              result(0, i_x) = 0.0;  // 1_x
+              result(0, i_y) = 0.0;  // 1_y
             }
             break;
+
           case 1:
-            // 1, x, y
-            for (index_t i = 0; i < n_points; i++) {
+            for (index_t i = 0; i < mu; i++) {
               auto p = points.row(i);
-              result(0, i) = 1.0;
-              result(1, i) = p(0);
-              result(2, i) = p(1);
+              result(0, i) = 1.0;   // 1
+              result(1, i) = p(0);  // x
+              result(2, i) = p(1);  // y
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto i_x = mu + 2 * i;
+              auto i_y = mu + 2 * i + 1;
+              result(0, i_x) = 0.0;  // 1_x
+              result(0, i_y) = 0.0;  // 1_y
+              result(1, i_x) = 1.0;  // x_x
+              result(1, i_y) = 0.0;  // x_y
+              result(2, i_x) = 0.0;  // y_x
+              result(2, i_y) = 1.0;  // y_y
             }
             break;
+
           case 2:
-            // 1, x, y, x^2, xy, y^2
-            for (index_t i = 0; i < n_points; i++) {
+            for (index_t i = 0; i < mu; i++) {
               auto p = points.row(i);
-              result(0, i) = 1.0;
-              result(1, i) = p(0);
-              result(2, i) = p(1);
-              result(3, i) = p(0) * p(0);
-              result(4, i) = p(0) * p(1);
-              result(5, i) = p(1) * p(1);
+              result(0, i) = 1.0;          // 1
+              result(1, i) = p(0);         // x
+              result(2, i) = p(1);         // y
+              result(3, i) = p(0) * p(0);  // x^2
+              result(4, i) = p(0) * p(1);  // xy
+              result(5, i) = p(1) * p(1);  // y^2
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto p = grad_points.row(i);
+              auto i_x = mu + 2 * i;
+              auto i_y = mu + 2 * i + 1;
+              result(0, i_x) = 0.0;         // 1_x
+              result(0, i_y) = 0.0;         // 1_y
+              result(1, i_x) = 1.0;         // x_x
+              result(1, i_y) = 0.0;         // x_y
+              result(2, i_x) = 0.0;         // y_x
+              result(2, i_y) = 1.0;         // y_y
+              result(3, i_x) = 2.0 * p(0);  // x^2_x
+              result(3, i_y) = 0.0;         // x^2_y
+              result(4, i_x) = p(1);        // xy_x
+              result(4, i_y) = p(0);        // xy_y
+              result(5, i_x) = 0.0;         // y^2_x
+              result(5, i_y) = 2.0 * p(1);  // y^2_y
             }
             break;
+
           default:
             POLATORY_UNREACHABLE();
             break;
@@ -90,37 +146,98 @@ class monomial_basis : public polynomial_basis_base {
       case 3:
         switch (degree()) {
           case 0:
-            // 1
-            for (index_t i = 0; i < n_points; i++) {
-              result(0, i) = 1.0;
+            for (index_t i = 0; i < mu; i++) {
+              result(0, i) = 1.0;  // 1
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto i_x = mu + 3 * i;
+              auto i_y = mu + 3 * i + 1;
+              auto i_z = mu + 3 * i + 2;
+              result(0, i_x) = 0.0;  // 1_x
+              result(0, i_y) = 0.0;  // 1_y
+              result(0, i_z) = 0.0;  // 1_z
             }
             break;
+
           case 1:
-            // 1, x, y, z
-            for (index_t i = 0; i < n_points; i++) {
+            for (index_t i = 0; i < mu; i++) {
               auto p = points.row(i);
-              result(0, i) = 1.0;
-              result(1, i) = p(0);
-              result(2, i) = p(1);
-              result(3, i) = p(2);
+              result(0, i) = 1.0;   // 1
+              result(1, i) = p(0);  // x
+              result(2, i) = p(1);  // y
+              result(3, i) = p(2);  // z
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto i_x = mu + 3 * i;
+              auto i_y = mu + 3 * i + 1;
+              auto i_z = mu + 3 * i + 2;
+              result(0, i_x) = 0.0;  // 1_x
+              result(0, i_y) = 0.0;  // 1_y
+              result(0, i_z) = 0.0;  // 1_z
+              result(1, i_x) = 1.0;  // x_x
+              result(1, i_y) = 0.0;  // x_y
+              result(1, i_z) = 0.0;  // x_z
+              result(2, i_x) = 0.0;  // y_x
+              result(2, i_y) = 1.0;  // y_y
+              result(2, i_z) = 0.0;  // y_z
+              result(3, i_x) = 0.0;  // z_x
+              result(3, i_y) = 0.0;  // z_y
+              result(3, i_z) = 1.0;  // z_z
             }
             break;
+
           case 2:
-            // 1, x, y, z, x^2, xy, xz, y^2, yz, z^2
-            for (index_t i = 0; i < n_points; i++) {
+            for (index_t i = 0; i < mu; i++) {
               auto p = points.row(i);
-              result(0, i) = 1.0;
-              result(1, i) = p(0);
-              result(2, i) = p(1);
-              result(3, i) = p(2);
-              result(4, i) = p(0) * p(0);
-              result(5, i) = p(0) * p(1);
-              result(6, i) = p(0) * p(2);
-              result(7, i) = p(1) * p(1);
-              result(8, i) = p(1) * p(2);
-              result(9, i) = p(2) * p(2);
+              result(0, i) = 1.0;          // 1
+              result(1, i) = p(0);         // x
+              result(2, i) = p(1);         // y
+              result(3, i) = p(2);         // z
+              result(4, i) = p(0) * p(0);  // x^2
+              result(5, i) = p(0) * p(1);  // xy
+              result(6, i) = p(0) * p(2);  // xz
+              result(7, i) = p(1) * p(1);  // y^2
+              result(8, i) = p(1) * p(2);  // yz
+              result(9, i) = p(2) * p(2);  // z^2
+            }
+            for (index_t i = 0; i < sigma; i++) {
+              auto p = grad_points.row(i);
+              auto i_x = mu + 3 * i;
+              auto i_y = mu + 3 * i + 1;
+              auto i_z = mu + 3 * i + 2;
+              result(0, i_x) = 0.0;         // 1_x
+              result(0, i_y) = 0.0;         // 1_y
+              result(0, i_z) = 0.0;         // 1_z
+              result(1, i_x) = 1.0;         // x_x
+              result(1, i_y) = 0.0;         // x_y
+              result(1, i_z) = 0.0;         // x_z
+              result(2, i_x) = 0.0;         // y_x
+              result(2, i_y) = 1.0;         // y_y
+              result(2, i_z) = 0.0;         // y_z
+              result(3, i_x) = 0.0;         // z_x
+              result(3, i_y) = 0.0;         // z_y
+              result(3, i_z) = 1.0;         // z_z
+              result(4, i_x) = 2.0 * p(0);  // x^2_x
+              result(4, i_y) = 0.0;         // x^2_y
+              result(4, i_z) = 0.0;         // x^2_z
+              result(5, i_x) = p(1);        // xy_x
+              result(5, i_y) = p(0);        // xy_y
+              result(5, i_z) = 0.0;         // xy_z
+              result(6, i_x) = p(2);        // xz_x
+              result(6, i_y) = 0.0;         // xz_y
+              result(6, i_z) = p(0);        // xz_z
+              result(7, i_x) = 0.0;         // y^2_x
+              result(7, i_y) = 2.0 * p(1);  // y^2_y
+              result(7, i_z) = 0.0;         // y^2_z
+              result(8, i_x) = 0.0;         // yz_x
+              result(8, i_y) = p(2);        // yz_y
+              result(8, i_z) = p(1);        // yz_z
+              result(9, i_x) = 0.0;         // z^2_x
+              result(9, i_y) = 0.0;         // z^2_y
+              result(9, i_z) = 2.0 * p(2);  // z^2_z
             }
             break;
+
           default:
             POLATORY_UNREACHABLE();
             break;
