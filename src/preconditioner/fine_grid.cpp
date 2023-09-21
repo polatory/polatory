bf438df1fbcccca157b32a2a240724578a453bad
unageek <29035331+unageek@users.noreply.github.com>
2023-09-21 15:52:47 +0900

    Add gradient data support to `{coarse,fine}_grid` (#161)

--- src/preconditioner/fine_grid.cpp
+++ src/preconditioner/fine_grid.cpp
@@ -1,16 +1,28 @@
 #include <polatory/common/macros.hpp>
 #include <polatory/preconditioner/fine_grid.hpp>
 
+#include "mat_a.hpp"
+
 namespace polatory::preconditioner {
 
 fine_grid::fine_grid(const model& model, const std::vector<index_t>& point_indices,
                      const std::vector<bool>& inner_point)
+    : fine_grid(model, point_indices, {}, inner_point, {}) {}
+
+fine_grid::fine_grid(const model& model, const std::vector<index_t>& point_indices,
+                     const std::vector<index_t>& grad_point_indices,
+                     const std::vector<bool>& inner_point,
+                     const std::vector<bool>& inner_grad_point)
     : model_(model),
       point_idcs_(point_indices),
+      grad_point_idcs_(grad_point_indices),
       inner_point_(inner_point),
+      inner_grad_point_(inner_grad_point),
       l_(model.poly_basis_size()),
-      m_(static_cast<index_t>(point_indices.size())) {
-  POLATORY_ASSERT(m_ > l_);
+      mu_(static_cast<index_t>(point_indices.size())),
+      sigma_(static_cast<index_t>(grad_point_indices.size())),
+      m_(mu_ + model.poly_dimension() * sigma_) {
+  POLATORY_ASSERT(mu_ > l_);
 }
 
 void fine_grid::clear() {
@@ -20,22 +32,21 @@ void fine_grid::clear() {
 
 void fine_grid::setup(const geometry::points3d& points_full,
                       const Eigen::MatrixXd& lagrange_pt_full) {
+  setup(points_full, geometry::points3d(0, 3), lagrange_pt_full);
+}
+
+void fine_grid::setup(const geometry::points3d& points_full,
+                      const geometry::points3d& grad_points_full,
+                      const Eigen::MatrixXd& lagrange_pt_full) {
   auto points = points_full(point_idcs_, Eigen::all);
-  auto lagrange_pt = lagrange_pt_full(Eigen::all, point_idcs_);
+  auto grad_points = grad_points_full(grad_point_idcs_, Eigen::all);
 
   // Compute A.
-  Eigen::MatrixXd a(m_, m_);
-  const auto& rbf = model_.rbf();
-  a.diagonal().array() = rbf.evaluate(geometry::vector3d::Zero()) + model_.nugget();
-  for (index_t i = 0; i < m_ - 1; i++) {
-    for (index_t j = i + 1; j < m_; j++) {
-      a(i, j) = rbf.evaluate(points.row(i) - points.row(j));
-      a(j, i) = a(i, j);
-    }
-  }
+  auto a = mat_a(model_, points, grad_points);
 
   if (l_ > 0) {
     // Compute -E.
+    auto lagrange_pt = lagrange_pt_full(Eigen::all, point_idcs_);
     me_ = -lagrange_pt.rightCols(m_ - l_);
 
     // Compute decomposition of Q^T A Q.
@@ -46,12 +57,23 @@ void fine_grid::setup(const geometry::points3d& points_full,
   } else {
     ldlt_of_qtaq_ = a.ldlt();
   }
+
+  mu_full_ = points_full.rows();
 }
 
 void fine_grid::set_solution_to(Eigen::Ref<common::valuesd> weights_full) const {
-  for (index_t i = 0; i < m_; i++) {
+  auto dim = model_.poly_dimension();
+
+  for (index_t i = 0; i < mu_; i++) {
     if (inner_point_.at(i)) {
       weights_full(point_idcs_.at(i)) = lambda_(i);
+    }
+  }
+
+  for (index_t i = 0; i < sigma_; i++) {
+    if (inner_grad_point_.at(i)) {
+      weights_full.segment(mu_full_ + dim * grad_point_idcs_.at(i), dim) =
+          lambda_.segment(mu_ + dim * i, dim);
     }
   }
 }
