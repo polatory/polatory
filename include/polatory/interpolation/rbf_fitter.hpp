78192c182887ccec9c83fec8bedb21fe665fdb7c
unageek <29035331+unageek@users.noreply.github.com>
2023-09-25 16:53:18 +0900

    WIP

--- include/polatory/interpolation/rbf_fitter.hpp
+++ include/polatory/interpolation/rbf_fitter.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <polatory/geometry/point3d.hpp>
+#include <polatory/interpolation/rbf_solver.hpp>
 #include <polatory/model.hpp>
 #include <polatory/types.hpp>
 
@@ -10,11 +11,13 @@ class rbf_fitter {
  public:
   rbf_fitter(const model& model, const geometry::points3d& points);
 
+  rbf_fitter(const model& model, const geometry::points3d& points,
+             const geometry::points3d& grad_points);
+
   common::valuesd fit(const common::valuesd& values, double absolute_tolerance, int max_iter) const;
 
  private:
-  const model& model_;
-  const geometry::points3d& points_;
+  rbf_solver solver_;
 };
 
 }  // namespace polatory::interpolation
--- src/interpolation/rbf_fitter.cpp
+++ src/interpolation/rbf_fitter.cpp
@@ -1,15 +1,17 @@
 #include <polatory/interpolation/rbf_fitter.hpp>
-#include <polatory/interpolation/rbf_solver.hpp>
 
 namespace polatory::interpolation {
 
 rbf_fitter::rbf_fitter(const model& model, const geometry::points3d& points)
-    : model_(model), points_(points) {}
+    : rbf_fitter(model, points, geometry::points3d(0, 3)) {}
+
+rbf_fitter::rbf_fitter(const model& model, const geometry::points3d& points,
+                       const geometry::points3d& grad_points)
+    : solver_(model, points, grad_points) {}
 
 common::valuesd rbf_fitter::fit(const common::valuesd& values, double absolute_tolerance,
                                 int max_iter) const {
-  rbf_solver solver(model_, points_);
-  return solver.solve(values, absolute_tolerance, max_iter);
+  return solver_.solve(values, absolute_tolerance, max_iter);
 }
 
 }  // namespace polatory::interpolation
