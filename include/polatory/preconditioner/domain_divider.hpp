c557f6d460fe053e542392d25d0b386629d5d6cc
unageek <29035331+unageek@users.noreply.github.com>
2023-10-21 02:29:30 +0900

    WIP

--- include/polatory/preconditioner/domain_divider.hpp
+++ include/polatory/preconditioner/domain_divider.hpp
@@ -18,7 +18,7 @@ namespace polatory::preconditioner {
 
 template <int Dim>
 class domain_divider {
-  static constexpr double kOverlapQuota = 0.25;
+  static constexpr double kOverlapQuota = 0.5;
   static constexpr index_t kMaxLeafSize = 256;
 
   using Bbox = geometry::bboxNd<Dim>;
@@ -32,10 +32,7 @@ class domain_divider {
                  const std::vector<index_t>& point_indices,
                  const std::vector<index_t>& grad_point_indices,
                  const std::vector<index_t>& poly_point_indices)
-      : points_(points),
-        grad_points_(grad_points),
-        mixed_size_of_root_(static_cast<index_t>(point_indices.size() + grad_point_indices.size())),
-        poly_point_idcs_(poly_point_indices) {
+      : points_(points), grad_points_(grad_points), poly_point_idcs_(poly_point_indices) {
     Domain root;
 
     root.point_indices = point_indices;
@@ -45,7 +42,6 @@ class domain_divider {
     root.inner_grad_point = std::vector<bool>(grad_point_indices.size(), true);
 
     root.bbox_ = domain_bbox(root);
-    longest_side_length_of_root_ = root.bbox_.width().maxCoeff();
 
     domains_.push_back(std::move(root));
 
@@ -117,7 +113,7 @@ class domain_divider {
     }
 
     auto split_axis = index_t{0};
-    auto longest_side_length = d.bbox_.width().maxCoeff(&split_axis);
+    d.bbox_.width().maxCoeff(&split_axis);
 
     // TODO(mizuno): Sort all points along each axis and cache the result as a permutation.
     std::sort(mixed_points.begin(), mixed_points.end(),
@@ -126,11 +122,8 @@ class domain_divider {
                        (b.grad ? grad_points_ : points_)(b.index, split_axis);
               });
 
-    auto q =
-        longest_side_length_of_root_ / longest_side_length *
-        std::sqrt(static_cast<double>(kMaxLeafSize) / static_cast<double>(mixed_size_of_root_)) *
-        kOverlapQuota;
-    q = std::min(0.5, q);
+    auto q = kOverlapQuota * static_cast<double>(kMaxLeafSize) /
+             static_cast<double>(mixed_points.size());
 
     auto n_pts = d.mixed_size();
     auto n_overlap_pts = static_cast<index_t>(round_half_to_even(q * static_cast<double>(n_pts)));
@@ -227,10 +220,7 @@ class domain_divider {
 
   const Points points_;
   const Points grad_points_;
-
-  index_t mixed_size_of_root_;
-  double longest_side_length_of_root_;
-  std::vector<index_t> poly_point_idcs_;
+  const std::vector<index_t> poly_point_idcs_;
   std::list<Domain> domains_;
 };
 
