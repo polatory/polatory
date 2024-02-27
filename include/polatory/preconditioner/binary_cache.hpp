3f241439b02d0cad2b7a067e2a2ea297ee3189b8
unageek <29035331+unageek@users.noreply.github.com>
2024-02-27 21:37:34 +0900

    Clean up preconditioner cache when process is terminated (#171)

---
+++ include/polatory/preconditioner/binary_cache.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <fcntl.h>
+#include <unistd.h>
+#endif
+
+#include <boost/filesystem.hpp>
+#include <mutex>
+#include <stdexcept>
+#include <vector>
+
+namespace polatory::preconditioner {
+
+class binary_cache {
+ public:
+  binary_cache() {
+    auto filename = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
+
+#ifdef _WIN32
+    file_ = ::CreateFileW(filename.c_str(), GENERIC_READ | GENERIC_WRITE, 0, nullptr, CREATE_NEW,
+                          FILE_FLAG_DELETE_ON_CLOSE, nullptr);
+    if (file_ == INVALID_HANDLE_VALUE) {
+      throw std::runtime_error("failed to open a temporary file");
+    }
+#else
+    file_ = ::open(filename.c_str(), O_RDWR | O_CREAT | O_EXCL);
+    if (file_ == -1) {
+      throw std::runtime_error("failed to open a temporary file");
+    }
+    ::unlink(filename.c_str());
+#endif
+
+    records_.push_back({0, 0});
+  }
+
+  ~binary_cache() {
+#ifdef _WIN32
+    ::CloseHandle(file_);
+#else
+    ::close(file_);
+#endif
+  }
+
+  void get(std::size_t id, void* data) const {
+    std::lock_guard lock(mutex_);
+
+    const auto& record = records_.at(id);
+
+#ifdef _WIN32
+    LARGE_INTEGER distance;
+    distance.QuadPart = record.offset;
+    ::SetFilePointerEx(file_, distance, nullptr, FILE_BEGIN);
+    ::ReadFile(file_, data, record.size, nullptr, nullptr);
+#else
+    ::lseek(file_, record.offset, SEEK_SET);
+    ::read(file_, data, record.size);
+#endif
+  }
+
+  std::size_t put(const void* data, std::size_t size) {
+    std::lock_guard lock(mutex_);
+
+#ifdef _WIN32
+    LARGE_INTEGER distance;
+    distance.QuadPart = 0;
+    ::SetFilePointerEx(file_, distance, nullptr, FILE_END);
+    ::WriteFile(file_, data, size, nullptr, nullptr);
+#else
+    ::lseek(file_, 0, SEEK_END);
+    ::write(file_, data, size);
+#endif
+
+    auto id = records_.size();
+    auto offset = records_.back().offset + records_.back().size;
+    records_.push_back({offset, size});
+    return id;
+  }
+
+ private:
+  struct record {
+    std::size_t offset;
+    std::size_t size;
+  };
+
+#ifdef _WIN32
+  HANDLE file_;
+#else
+  int file_;
+#endif
+
+  std::vector<record> records_;
+  mutable std::mutex mutex_;
+};
+
+}  // namespace polatory::preconditioner
--- include/polatory/preconditioner/fine_grid.hpp
+++ include/polatory/preconditioner/fine_grid.hpp
@@ -2,12 +2,10 @@
 
 #include <Eigen/Cholesky>
 #include <Eigen/Core>
-#include <boost/filesystem.hpp>
-#include <fstream>
-#include <iostream>
 #include <polatory/common/macros.hpp>
 #include <polatory/geometry/point3d.hpp>
 #include <polatory/model.hpp>
+#include <polatory/preconditioner/binary_cache.hpp>
 #include <polatory/preconditioner/domain.hpp>
 #include <polatory/types.hpp>
 #include <utility>
@@ -41,21 +39,19 @@ class fine_grid {
   using Points = geometry::pointsNd<kDim>;
 
  public:
-  fine_grid(const Model& model, Domain&& domain)
+  fine_grid(const Model& model, Domain&& domain, binary_cache& cache)
       : model_(model),
         point_idcs_(std::move(domain.point_indices)),
         grad_point_idcs_(std::move(domain.grad_point_indices)),
         inner_point_(std::move(domain.inner_point)),
         inner_grad_point_(std::move(domain.inner_grad_point)),
+        cache_(cache),
         l_(model.poly_basis_size()),
         mu_(static_cast<index_t>(point_idcs_.size())),
         sigma_(static_cast<index_t>(grad_point_idcs_.size())),
-        m_(mu_ + kDim * sigma_),
-        filename_{boost::filesystem::temp_directory_path() / boost::filesystem::unique_path()} {
+        m_(mu_ + kDim * sigma_) {
     POLATORY_ASSERT(mu_ > l_);
   }
-
-  ~fine_grid() { boost::filesystem::remove(filename_); }
 
   void setup(const Points& points_full, const Points& grad_points_full,
              const Eigen::MatrixXd& lagrange_pt_full) {
@@ -147,28 +143,14 @@ class fine_grid {
 
  private:
   void load_ldlt_of_qtaq() {
-    std::ifstream ifs(filename_.string(), std::ios::binary);
-
-    if (!ifs) {
-      std::cerr << "Failed to open file: " << filename_ << std::endl;
-      std::exit(EXIT_FAILURE);
-    }
-
     auto& ldlt = ldlt_of_qtaq_.matrixLDLT();
     ldlt.resize(m_ - l_, m_ - l_);
-    ifs.read(reinterpret_cast<char*>(ldlt.data()), ldlt.size() * sizeof(double));
+    cache_.get(cache_id_, ldlt.data());
   }
 
   void save_ldlt_of_qtaq() {
-    std::ofstream ofs(filename_.string(), std::ios::binary);
-
-    if (!ofs) {
-      std::cerr << "Failed to open file: " << filename_ << std::endl;
-      std::exit(EXIT_FAILURE);
-    }
-
     auto& ldlt = ldlt_of_qtaq_.matrixLDLT();
-    ofs.write(reinterpret_cast<const char*>(ldlt.data()), ldlt.size() * sizeof(double));
+    cache_id_ = cache_.put(ldlt.data(), ldlt.size() * sizeof(double));
     ldlt.resize(0, 0);
   }
 
@@ -177,6 +159,8 @@ class fine_grid {
   const std::vector<index_t> grad_point_idcs_;
   const std::vector<bool> inner_point_;
   const std::vector<bool> inner_grad_point_;
+  binary_cache& cache_;
+  std::size_t cache_id_;
 
   const index_t l_;
   const index_t mu_;
@@ -193,8 +177,6 @@ class fine_grid {
 
   // Current solution.
   common::valuesd lambda_;
-
-  boost::filesystem::path filename_;
 };
 
 }  // namespace polatory::preconditioner
