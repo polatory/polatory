811a30f8adacdd641d86268db927ed1122357ff3
unageek <29035331+unageek@users.noreply.github.com>
2024-03-17 17:48:10 +0900

    Add more to bindings

--- python/python_binding.cpp
+++ python/python_binding.cpp
@@ -14,6 +14,11 @@ using namespace polatory;
 namespace py = pybind11;
 using namespace py::literals;
 
+template <int Dim>
+geometry::bboxNd<Dim> bbox_from_points(const geometry::pointsNd<Dim>& points) {
+  return geometry::bboxNd<Dim>::from_points(points);
+}
+
 template <int Dim, class Rbf>
 void define_rbf(py::module& m, const std::string& name) {
   py::class_<Rbf, rbf::rbf_proxy<Dim>>(m, name.c_str())
@@ -22,6 +27,14 @@ void define_rbf(py::module& m, const std::string& name) {
 
 template <int Dim>
 void define_module(py::module& m) {
+  py::class_<geometry::bboxNd<Dim>>(m, "Bbox")
+      .def(py::init<>())
+      .def(py::init<const geometry::pointNd<Dim>&, const geometry::pointNd<Dim>&>(), "min"_a,
+           "max"_a)
+      .def_static("from_points", &bbox_from_points<Dim>, "points"_a)
+      .def_property_readonly("min", &geometry::bboxNd<Dim>::min)
+      .def_property_readonly("max", &geometry::bboxNd<Dim>::max);
+
   py::class_<rbf::rbf_proxy<Dim>>(m, "Rbf")
       .def_property("anisotropy", &rbf::rbf_proxy<Dim>::anisotropy,
                     &rbf::rbf_proxy<Dim>::set_anisotropy)
@@ -109,6 +122,49 @@ PYBIND11_MODULE(_core, m) {
   define_module<1>(p1);
   define_module<2>(p2);
   define_module<3>(p3);
+
+  py::class_<point_cloud::normal_estimator>(m, "NormalEstimator")
+      .def(py::init<const geometry::points3d&>(), "points"_a)
+      .def("estimate_with_knn",
+           py::overload_cast<index_t, double>(&point_cloud::normal_estimator::estimate_with_knn),
+           "k"_a, "plane_factor_threshold"_a = 1.8)
+      .def("estimate_with_knn",
+           py::overload_cast<const std::vector<index_t>&, double>(
+               &point_cloud::normal_estimator::estimate_with_knn),
+           "ks"_a, "plane_factor_threshold"_a = 1.8)
+      .def("estimate_with_radius", &point_cloud::normal_estimator::estimate_with_radius, "radius"_a,
+           "plane_factor_threshold"_a = 1.8)
+      .def("orient_by_outward_vector", &point_cloud::normal_estimator::orient_by_outward_vector,
+           "v"_a)
+      .def("orient_closed_surface", &point_cloud::normal_estimator::orient_closed_surface, "k"_a);
+
+  py::class_<point_cloud::sdf_data_generator>(m, "SdfDataGenerator")
+      .def(
+          py::init<const geometry::points3d&, const geometry::vectors3d&, double, double, double>(),
+          "points"_a, "normals"_a, "min_distance"_a, "max_distance"_a, "multiplication"_a = 2.0)
+      .def_property_readonly("sdf_points", &point_cloud::sdf_data_generator::sdf_points)
+      .def_property_readonly("sdf_values", &point_cloud::sdf_data_generator::sdf_values);
+
+  py::class_<isosurface::field_function>(m, "_FieldFunction");
+
+  py::class_<isosurface::rbf_field_function, isosurface::field_function>(m, "RbfFieldFunction")
+      .def(py::init<interpolant<3>&>(), "interpolant"_a);
+
+  py::class_<isosurface::rbf_field_function_25d, isosurface::field_function>(m,
+                                                                             "RbfFieldFunction25d")
+      .def(py::init<interpolant<2>&>(), "interpolant"_a);
+
+  py::class_<isosurface::isosurface>(m, "Isosurface")
+      .def(py::init<const geometry::bbox3d&, double>(), "bbox"_a, "resolution"_a)
+      .def("generate", &isosurface::isosurface::generate, "field_fn"_a, "isovalue"_a = 0.0,
+           "refine"_a = true)
+      .def("generate_from_seed_points", &isosurface::isosurface::generate_from_seed_points,
+           "seed_points"_a, "field_fn"_a, "isovalue"_a = 0.0, "refine"_a = true);
+
+  py::class_<isosurface::surface>(m, "Surface")
+      .def("export_obj", &isosurface::surface::export_obj, "filename"_a)
+      .def_property_readonly("faces", &isosurface::surface::faces)
+      .def_property_readonly("vertices", &isosurface::surface::vertices);
 
   m.attr("__version__") = xstr(POLATORY_VERSION);
 }
