411b6aef4d6c294c6bebba68f58d4452d4edb012
unageek <29035331+unageek@users.noreply.github.com>
2024-07-21 01:02:32 +0900

    Anisotropic SDF data generation

--- cli/normals_to_sdf_command.cpp
+++ cli/normals_to_sdf_command.cpp
@@ -1,9 +1,12 @@
 #include <Eigen/Core>
 #include <algorithm>
+#include <boost/any.hpp>
 #include <boost/program_options.hpp>
+#include <cmath>
 #include <format>
 #include <numeric>
 #include <polatory/polatory.hpp>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -12,11 +15,12 @@
 using polatory::index_t;
 using polatory::matrixd;
 using polatory::read_table;
-using polatory::vectord;
 using polatory::write_table;
 using polatory::common::concatenate_cols;
+using polatory::geometry::matrix3d;
 using polatory::geometry::points3d;
 using polatory::geometry::vectors3d;
+using polatory::numeric::to_double;
 using polatory::point_cloud::sdf_data_generator;
 
 namespace {
@@ -25,28 +29,32 @@ struct options {
   std::string in_file;
   double min_offset{};
   double max_offset{};
-  double mult{};
+  double ratio{};
+  matrix3d aniso{};
   std::string out_file;
 };
 
 void run_impl(const options& opts) {
   matrixd table = read_table(opts.in_file);
 
-  // Shuffle the points so that the off-surface points will not be clustered.
-  std::vector<index_t> indices(table.rows());
-  std::iota(indices.begin(), indices.end(), index_t{0});
+  points3d points = table(Eigen::all, {0, 1, 2});
+  vectors3d normals = table(Eigen::all, {3, 4, 5});
+
+  auto n_normals = normals.rows();
+  auto n_normals_to_keep = static_cast<index_t>(std::round(opts.ratio * n_normals));
+  std::vector<index_t> indices(n_normals);
+  std::iota(indices.begin(), indices.end(), 0);
+  // Prevent clustering of off-surface points.
   std::shuffle(indices.begin(), indices.end(), std::mt19937{});
+  indices.resize(n_normals - n_normals_to_keep);
+  normals(indices, Eigen::all) *= 0.0;
 
-  points3d surface_points = table(indices, {0, 1, 2});
-  vectors3d surface_normals = table(indices, {3, 4, 5});
+  sdf_data_generator sdf_data(points, normals, opts.min_offset, opts.max_offset, opts.aniso);
 
-  sdf_data_generator sdf_data(surface_points, surface_normals, opts.min_offset, opts.max_offset,
-                              opts.mult);
+  const auto& sdf_points = sdf_data.sdf_points();
+  const auto& sdf_values = sdf_data.sdf_values();
 
-  const auto& points = sdf_data.sdf_points();
-  const auto& values = sdf_data.sdf_values();
-
-  write_table(opts.out_file, concatenate_cols(points, values));
+  write_table(opts.out_file, concatenate_cols(sdf_points, sdf_values));
 }
 
 }  // namespace
@@ -65,8 +73,14 @@ void normals_to_sdf_command::run(const std::vector<std::string>& args,
        "Minimum offset distance of off-surface points")  //
       ("offset", po::value(&opts.max_offset)->required()->value_name("OFFSET"),
        "Default offset distance of off-surface points")  //
-      ("mult", po::value(&opts.mult)->default_value(2.0, "2.0")->value_name("1.0 to 3.0"),
-       "The size of output data as a multiple the input data")  //
+      ("aniso",
+       po::value(&opts.aniso)
+           ->multitoken()
+           ->default_value(matrix3d::Identity(), "1 0 0 0 1 0 0 0 1")
+           ->value_name("A_11 A_12 ... A_33"),
+       "Elements of the anisotropy matrix")  //
+      ("ratio", po::value(&opts.ratio)->default_value(0.5, "0.5")->value_name("0.0 to 1.0"),
+       "Ratio of normals to use for generating off-surface points")  //
       ("out", po::value(&opts.out_file)->required()->value_name("FILE"),
        "Output file in CSV format:\n  X,Y,Z,VAL")  //
       ;
@@ -89,5 +103,28 @@ void normals_to_sdf_command::run(const std::vector<std::string>& args,
     throw;
   }
 
+  if (!(opts.ratio >= 0.0 && opts.ratio <= 1.0)) {
+    throw std::runtime_error("--ratio must be within 0.0 to 1.0");
+  }
+
   run_impl(opts);
 }
+
+namespace Eigen {
+
+inline void validate(boost::any& v, const std::vector<std::string>& values, matrix3d*, int) {
+  namespace po = boost::program_options;
+
+  if (values.size() != 9) {
+    throw po::validation_error(po::validation_error::invalid_option_value);
+  }
+
+  matrix3d aniso;
+  aniso << to_double(values.at(0)), to_double(values.at(1)), to_double(values.at(2)),
+      to_double(values.at(3)), to_double(values.at(4)), to_double(values.at(5)),
+      to_double(values.at(6)), to_double(values.at(7)), to_double(values.at(8));
+
+  v = aniso;
+}
+
+}  // namespace Eigen
--- src/point_cloud/sdf_data_generator.cpp
+++ src/point_cloud/sdf_data_generator.cpp
@@ -1,12 +1,20 @@
+#include <Eigen/LU>
 #include <polatory/point_cloud/kdtree.hpp>
 #include <polatory/point_cloud/sdf_data_generator.hpp>
 #include <stdexcept>
+#include <vector>
 
 namespace polatory::point_cloud {
 
 sdf_data_generator::sdf_data_generator(const geometry::points3d& points,
                                        const geometry::vectors3d& normals, double min_distance,
-                                       double max_distance, double multiplication) {
+                                       double max_distance)
+    : sdf_data_generator(points, normals, min_distance, max_distance,
+                         geometry::matrix3d::Identity()) {}
+
+sdf_data_generator::sdf_data_generator(const geometry::points3d& points,
+                                       const geometry::vectors3d& normals, double min_distance,
+                                       double max_distance, const geometry::matrix3d& aniso) {
   if (normals.rows() != points.rows()) {
     throw std::invalid_argument("normals.rows() must be equal to points.rows()");
   }
@@ -15,27 +23,46 @@ sdf_data_generator::sdf_data_generator(const geometry::points3d& points,
     throw std::invalid_argument("min_distance must be less than or equal to max_distance");
   }
 
-  if (!(multiplication > 1.0 && multiplication <= 3.0)) {
-    throw std::invalid_argument("multiplication must be within (1.0, 3.0]");
+  if (!(aniso.determinant() > 0.0)) {
+    throw std::invalid_argument("aniso must have a positive determinant");
   }
 
+  if (aniso.isIdentity()) {
+    auto [sdf_points, sdf_values] = estimate_impl(points, normals, min_distance, max_distance);
+    sdf_points_ = sdf_points;
+    sdf_values_ = sdf_values;
+  } else {
+    auto a_points = geometry::transform_points<3>(aniso, points);
+    auto a_normals = geometry::transform_vectors<3>(aniso.inverse().transpose(), normals);
+    for (auto n : a_normals.rowwise()) {
+      if (!n.isZero()) {
+        n = n.normalized();
+      }
+    }
+    auto [sdf_points, sdf_values] = estimate_impl(a_points, a_normals, min_distance, max_distance);
+    sdf_points_ = geometry::transform_points<3>(aniso.inverse(), sdf_points);
+    sdf_values_ = sdf_values;
+  }
+}
+
+std::pair<geometry::points3d, vectord> sdf_data_generator::estimate_impl(
+    const geometry::points3d& points, const geometry::vectors3d& normals, double min_distance,
+    double max_distance) {
   kdtree tree(points);
 
   auto n_points = points.rows();
-  auto n_reduced_points =
-      static_cast<index_t>(((multiplication - 1.0) / 2.0) * static_cast<double>(n_points));
-  auto n_max_sdf_points = n_points + 2 * n_reduced_points;
+  auto n_max_sdf_points = 3 * n_points;
   auto n_sdf_points = n_points;
 
-  sdf_points_ = geometry::points3d(n_max_sdf_points, 3);
-  sdf_points_.topRows(n_points) = points;
-  sdf_values_ = vectord::Zero(n_max_sdf_points);
+  geometry::points3d sdf_points(n_max_sdf_points, 3);
+  sdf_points.topRows(n_points) = points;
+  vectord sdf_values = vectord::Zero(n_max_sdf_points);
 
   std::vector<index_t> nn_indices;
   std::vector<double> nn_distances;
 
-  for (auto sign : {1.0, -1.0}) {
-    for (index_t i = 0; i < n_reduced_points; i++) {
+  for (auto sign : {-1.0, 1.0}) {
+    for (index_t i = 0; i < n_points; i++) {
       auto p = points.row(i);
       auto n = normals.row(i);
 
@@ -69,14 +96,16 @@ sdf_data_generator::sdf_data_generator(const geometry::points3d& points,
         continue;
       }
 
-      sdf_points_.row(n_sdf_points) = q;
-      sdf_values_(n_sdf_points) = sign * d;
+      sdf_points.row(n_sdf_points) = q;
+      sdf_values(n_sdf_points) = sign * d;
       n_sdf_points++;
     }
   }
 
-  sdf_points_.conservativeResize(n_sdf_points, 3);
-  sdf_values_.conservativeResize(n_sdf_points);
+  sdf_points.conservativeResize(n_sdf_points, 3);
+  sdf_values.conservativeResize(n_sdf_points);
+
+  return {sdf_points, sdf_values};
 }
 
 const geometry::points3d& sdf_data_generator::sdf_points() const { return sdf_points_; }
