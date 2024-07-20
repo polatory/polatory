cffaf6b8d400d5e7809e3e0827418ed70ed439bc
unageek <29035331+unageek@users.noreply.github.com>
2024-07-21 02:10:33 +0900

    Anisotropic SDF data generation (#191)

--- include/polatory/point_cloud/sdf_data_generator.hpp
+++ include/polatory/point_cloud/sdf_data_generator.hpp
@@ -2,6 +2,7 @@
 
 #include <polatory/geometry/point3d.hpp>
 #include <polatory/types.hpp>
+#include <utility>
 
 namespace polatory::point_cloud {
 
@@ -9,13 +10,21 @@ namespace polatory::point_cloud {
 class sdf_data_generator {
  public:
   sdf_data_generator(const geometry::points3d& points, const geometry::vectors3d& normals,
-                     double min_distance, double max_distance, double multiplication = 2.0);
+                     double min_distance, double max_distance);
+
+  sdf_data_generator(const geometry::points3d& points, const geometry::vectors3d& normals,
+                     double min_distance, double max_distance, const geometry::matrix3d& aniso);
 
   const geometry::points3d& sdf_points() const;
 
   const vectord& sdf_values() const;
 
  private:
+  static std::pair<geometry::points3d, vectord> estimate_impl(const geometry::points3d& points,
+                                                              const geometry::vectors3d& normals,
+                                                              double min_distance,
+                                                              double max_distance);
+
   geometry::points3d sdf_points_;
   vectord sdf_values_;
 };
